1b452a28b8b7cfe085a04e9ef942137a30faac01
prasadbanuka <banuka.mawelage@gmail.com>
2021-12-22 11:41:03 +0530

    aatapi home page

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -2,39 +2,189 @@
 
 @section('title', app_name() . ' | ' . __('navs.general.home'))
 
+@push('after-styles')
+    <link rel="stylesheet" href="{{ url('css/index.css') }}">
+    <link rel="stylesheet" href="{{ url('css/style.css') }}">
+@endpush
+
+
 @section('content')
-    <div class="row mb-4">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">
-                    <i class="fas fa-home"></i> @lang('navs.general.home')
-                </div>
-                <div class="card-body">
-                    @lang('strings.frontend.welcome_to', ['place' => app_name()])
-                </div>
-            </div><!--card-->
-        </div><!--col-->
-    </div><!--row-->
 
-    <div class="row mb-4">
-        <div class="col">
-            <example-component></example-component>
-        </div><!--col-->
-    </div><!--row-->
+<section id="home-banner">
+    <div class="banner-txt-wrapper">
+        <h1>AATAAPI</h1>
+        <h4 style="font-weight:300;">Center for Mindfulness & Relaxation</h4>
+    </div>
+    <div class="logo-wrapper">
+        <img class="logo" src="{{ url('img/frontend/logo.png') }}" alt="logo">
+    </div>
+</section>
+</div> 
+<!-- end of container fluid -->
 
-    <div class="row">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">
-                    <i class="fab fa-font-awesome-flag"></i> Font Awesome @lang('strings.frontend.test')
+<div class="container-fluid below-banner-all">
+<a class="donate-link" href="#" data-toggle="modal" data-target="#donate">
+    <img class="sonar-wave" width="80px" src="{{ url('img/frontend/donate-logo.png') }}" alt="donate logo">
+    <div style="width:80px; height:80px; background:#FFCC00;border-radius:50%; position: absolute; top: 30px; right: 15px;z-index:-1" class="round-shape"></div>
+</a>
+
+
+
+<section class="below-banner-section">
+    <div class="container">
+        <!-- desktop design -->
+        <div class="inner-container-home-first hidden-xs">
+            <img class="home-first-img col-xs-12" src="{{ url('img/frontend/home/home-first.png') }}" alt="">
+            <div class="home-first-card col-xs-12">
+                <h2 class="light-title"><span class="small-title">About the </span>Center</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
+            </div>
+        </div>
+
+        <!-- mobile design -->
+        <div class="inner-container-home-first visible-xs">
+            <img class="home-first-img col-xs-12" src="{{ url('img/frontend/home/home-first.png') }}" alt="">
+            <div class="home-mobile-first-card col-xs-12">
+                <h2 class="light-title"><span class="small-title">About the </span>Center</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
+            </div>
+        </div>
+
+
+
+    </div>
+ 
+     
+
+
+</section>
+<section id="story">
+    <div class="container-fluid full-width-container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-xs-12 col-sm-12">
+                    <img class="side-img" src="{{ url('img/frontend/home/bo-leaf.png') }}" alt="">
                 </div>
-                <div class="card-body">
-                    <i class="fas fa-home"></i>
-                    <i class="fab fa-facebook"></i>
-                    <i class="fab fa-twitter"></i>
-                    <i class="fab fa-pinterest"></i>
-                </div><!--card-body-->
-            </div><!--card-->
-        </div><!--col-->
-    </div><!--row-->
+
+                <div class="col-md-6 col-xs-12 col-sm-12">
+                    <div class="txt-wrapper">
+                        <h2 style="color:#fff;">The Story</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Egestas fringilla phasellus faucibus scelerisque eleifend
+                            donec pretium. Viverra accumsan in nisl nisi scelerisque. Nec feugiat nisl pretium fusce id
+                            velit ut tortor. Nibh tellus molestie nunc non blandit. Elit at imperdiet dui accumsan sit
+                            amet nulla. Faucibus in ornare quam viverra orci sagittis eu volutpat. Pharetra convallis
+                            posuere morbi leo urna molestie at elementum. A scelerisque purus semper eget.</p>
+                        <p>Et netus et malesuada fames ac. Egestas tellus rutrum tellus pellentesque eu tincidunt. Ac
+                            orci phasellus egestas tellus rutrum tellus pellentesque. Dictum at tempor commodo
+                            ullamcorper a lacus vestibulum sed arcu. Nibh venenatis cras sed felis eget velit aliquet.
+                            Sem et tortor consequat id porta nibh venenatis cras sed. Proin nibh nisl condimentum id
+                            venenatis a condimentum. Cursus turpis massa tincidunt dui ut ornare lectus sit amet.
+                            Viverra maecenas accumsan lacus vel facilisis volutpat est velit egestas.</p>
+
+                        <a class="simple-links" href="">View more images <i style="margin-left:5px;"
+                                class="fas fa-arrow-right"></i></a>
+                    </div>
+                </div>
+
+
+            </div>
+            <div class="row">
+                <div class="img-row">
+                    <img class="img-row-image" src="{{ url('img/frontend/home/part-1.png') }}" alt="">
+                    <img class="img-row-image" src="{{ url('img/frontend/home/part-2.png') }}" alt="">
+                    <img class="img-row-image" src="{{ url('img/frontend/home/part-3.png') }}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="program-slider">
+    <img style="text-align: center;display:block; margin-left:auto;margin-right:auto; width:100%;"
+        src="{{ url('img/frontend/home/topic-here.png') }}">
+</section>
+<section>
+    <div class="container mobile-container">
+        <h2 style="text-align:center;margin-bottom: 45px;">Latest at <span class="highlighted-title">AATHAPI</span></h2>
+        <div class="row align-items-center full-card">
+            <div class="col-md-6 col-xs-12 padding-fix">
+                <img class="large-img" src="{{ url('img/frontend/home/hill.png') }}" alt="hill">
+            </div>
+            <div class="col-md-6 col-xs-12 txt-padding-fix">
+                <h3>Sample Title Here</h3>
+                <p class="date">02<sup>nd</sup> January 2022</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.</p>
+                <a class="links" href="">Read More<i class="btn-icon fas fa-arrow-right"></i></a>
+                <div class="slider-nav">
+                    <a href=""><img src="{{ url('img/frontend/home/left.png') }}" alt=""
+                            style="width:25px; margin-right:10px;"></a>
+                    <a href=""><img src="{{ url('img/frontend/home/right.png') }}" alt="" style="width:25px;"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section>
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-4 col-xs-12">
+                <div class="card">
+                    <img src="{{ url('img/frontend/home/card1.png') }}" alt="event-one" style="width:100%">
+                    <div class="container">
+                        <h3>SAMPLE TITLE HERE</h3>
+                        <p class="date">02<sup>nd</sup> January 2022</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 col-xs-12">
+                <div class="card">
+                    <img src="{{ url('img/frontend/home/card2.png') }}" alt="event-two" style="width:100%">
+                    <div class="container">
+                        <h3>SAMPLE TITLE HERE</h3>
+                        <p class="date">02<sup>nd</sup> January 2022</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 col-xs-12">
+                <div class="card">
+                    <img src="{{ url('img/frontend/home/card3.png') }}" alt="event-three" style="width:100%">
+                    <div class="container">
+                        <h3>SAMPLE TITLE HERE</h3>
+                        <p class="date">02<sup>nd</sup> January 2022</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+</div>
 @endsection
--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -1,47 +1,70 @@
 <!DOCTYPE html>
 @langrtl
-    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="rtl">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="rtl">
 @else
-    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 @endlangrtl
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>@yield('title', app_name())</title>
-        <meta name="description" content="@yield('meta_description', 'Laravel Boilerplate')">
-        <meta name="author" content="@yield('meta_author', 'Anthony Rappa')">
-        @yield('meta')
 
-        {{-- See https://laravel.com/docs/5.5/blade#stacks for usage --}}
-        @stack('before-styles')
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title', app_name())</title>
+    <meta name="description" content="@yield('meta_description', 'Laravel Boilerplate')">
+    <meta name="author" content="@yield('meta_author', 'Anthony Rappa')">
 
-        <!-- Check if the language is set to RTL, so apply the RTL layouts -->
-        <!-- Otherwise apply the normal LTR layouts -->
-        {{ style(mix('css/frontend.css')) }}
+    @yield('meta')
 
-        @stack('after-styles')
-    </head>
-    <body>
-        @include('includes.partials.read-only')
+    {{-- See https://laravel.com/docs/5.5/blade#stacks for usage --}}
+    @stack('before-styles')
 
-        <div id="app">
-            @include('includes.partials.logged-in-as')
-            @include('frontend.includes.nav')
+    <!-- Check if the language is set to RTL, so apply the RTL layouts -->
+    <!-- Otherwise apply the normal LTR layouts -->
+    {{ style(mix('css/frontend.css')) }}
+    <!-- external links -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{ url('css/style.css') }}">
+    <!-- fontawesome -->
+    <script src="https://kit.fontawesome.com/aa4e69f91b.js" crossorigin="anonymous"></script>
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
+    @stack('after-styles')
+</head>
 
-            <div class="container">
-                @include('includes.partials.messages')
-                @yield('content')
-            </div><!-- container -->
-        </div><!-- #app -->
+<body>
+    @include('includes.partials.read-only')
 
-        <!-- Scripts -->
-        @stack('before-scripts')
-        {!! script(mix('js/manifest.js')) !!}
-        {!! script(mix('js/vendor.js')) !!}
-        {!! script(mix('js/frontend.js')) !!}
-        @stack('after-scripts')
+    <div id="app">
+        @include('includes.partials.logged-in-as')
+        @include('frontend.includes.nav')
+        <div class="container-fluid full-width-container-fluid">
+            @include('includes.partials.messages')
+            @yield('content')
 
-        @include('includes.partials.ga')
-    </body>
+            @include('frontend.includes.footer')
+
+        </div><!-- container -->
+    </div><!-- #app -->
+
+    <!-- Scripts -->
+    @stack('before-scripts')
+    {!! script(mix('js/manifest.js')) !!}
+    {!! script(mix('js/vendor.js')) !!}
+    {!! script(mix('js/frontend.js')) !!}
+    <!-- external links -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    @stack('after-scripts')
+
+
+
+    @include('includes.partials.ga')
+</body>
+
 </html>
